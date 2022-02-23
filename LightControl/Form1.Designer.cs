abd0ee0f515a7637760ef08cbf4127b3417f958a
KimHakdo <khd5675@gmail.com>
2022-02-23 17:33:35 +0900

    초기 커밋.

---
+++ LightControl/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace LightControl
+{
+    partial class connectState
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        /*
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }*/
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.portNumber = new System.Windows.Forms.ComboBox();
+            this.lightValue = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.lightKind = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.portState = new System.Windows.Forms.Label();
+            this.dataShowBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // portNumber
+            // 
+            this.portNumber.FormattingEnabled = true;
+            this.portNumber.Location = new System.Drawing.Point(12, 23);
+            this.portNumber.Name = "portNumber";
+            this.portNumber.Size = new System.Drawing.Size(121, 20);
+            this.portNumber.TabIndex = 1;
+            // 
+            // lightValue
+            // 
+            this.lightValue.Location = new System.Drawing.Point(139, 68);
+            this.lightValue.Name = "lightValue";
+            this.lightValue.Size = new System.Drawing.Size(100, 21);
+            this.lightValue.TabIndex = 2;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(245, 67);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "송신";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.SendData);
+            // 
+            // lightKind
+            // 
+            this.lightKind.FormattingEnabled = true;
+            this.lightKind.Location = new System.Drawing.Point(12, 67);
+            this.lightKind.Name = "lightKind";
+            this.lightKind.Size = new System.Drawing.Size(121, 20);
+            this.lightKind.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Connect";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(139, 23);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 7;
+            this.connectButton.Text = "연결";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.PortConnet);
+            // 
+            // portState
+            // 
+            this.portState.AutoSize = true;
+            this.portState.Location = new System.Drawing.Point(220, 31);
+            this.portState.Name = "portState";
+            this.portState.Size = new System.Drawing.Size(57, 12);
+            this.portState.TabIndex = 8;
+            this.portState.Text = "연결 상태";
+            // 
+            // dataShowBox
+            // 
+            this.dataShowBox.Location = new System.Drawing.Point(14, 96);
+            this.dataShowBox.Name = "dataShowBox";
+            this.dataShowBox.Size = new System.Drawing.Size(503, 131);
+            this.dataShowBox.TabIndex = 9;
+            this.dataShowBox.Text = "";
+            // 
+            // connectState
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 239);
+            this.Controls.Add(this.dataShowBox);
+            this.Controls.Add(this.portState);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lightKind);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.lightValue);
+            this.Controls.Add(this.portNumber);
+            this.Name = "connectState";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox portNumber;
+        private System.Windows.Forms.TextBox lightValue;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.ComboBox lightKind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connectButton;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label portState;
+        private System.Windows.Forms.RichTextBox dataShowBox;
+    }
+}
+
---
+++ LightControl/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace LightControl
+{
+    public partial class connectState : Form
+    {
+        public string receiveData;
+                                                      //채널 1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 각 채널의 값 0 ~ 255(00 ~ FF) 설정, 초기값 모든채널 0
+        public byte[] sendData = { 0x01, 0x00, 0x10, 0x29, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04 };
+        public connectState()
+        {
+            InitializeComponent();
+            portNumber.DataSource = SerialPort.GetPortNames();
+            lightKind.Items.Add("Off");
+            lightKind.Items.Add("Ring");
+            lightKind.Items.Add("Bar");
+            lightKind.Items.Add("Coxaml");
+        }
+
+        //Port Connect
+        private void PortConnet(object sender, EventArgs e)
+        {
+            if(!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = portNumber.Text;
+                serialPort1.BaudRate = 19200;
+                serialPort1.DataBits = 8;
+                serialPort1.Parity = Parity.None;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(SerialPort1_DataReceived);
+
+                serialPort1.Open();
+                portState.Text = "연결됨";
+                portNumber.Enabled = false;
+            }
+            else
+            {
+                serialPort1.Close();
+                portState.Text = "연결 해제";
+                portNumber.Enabled = true;
+            }
+        }
+
+        //데이터 수신
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Invoke(new EventHandler(MySerialReceived));
+        }        
+        private void MySerialReceived(object s, EventArgs e)
+        {
+            int receiveData = serialPort1.ReadByte();
+            dataShowBox.Text += string.Format("{0:X2}", receiveData) + " ";
+            dataShowBox.Text += "\n";
+        }
+
+        private void SendData(object sender, EventArgs e)
+        {
+            this.sendData = SendLightData(this.sendData, lightKind.Text);
+            serialPort1.Write(sendData,0,sendData.Length);
+            foreach(byte data in sendData) dataShowBox.Text += data.ToString("X2") + " ";
+            dataShowBox.Text += "\n";
+
+        }
+        private byte[] SendLightData(byte[] sendData, string light)
+        {
+            int hex = int.Parse(lightValue.Text);
+            byte value = Convert.ToByte(hex);
+            value = value > (byte)180 ? (byte)180 : value;
+
+            switch (light)
+            {
+                case "Ring":
+                    sendData[4] = value;
+                    break;
+                case "Bar":
+                    sendData[5] = value;
+                    break;
+                case "Coxaml":
+                    sendData[7] = value;
+                    break;
+                case "Off":
+                    sendData[4] = 0;
+                    sendData[5] = 0;
+                    sendData[7] = 0;
+                    break;
+            }
+
+            return sendData;
+        }
+    }
+}
